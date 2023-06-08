ab6ce9ab683371e1319167c6e9549c9cdb63b5c2
Zhenwen Liang <525655072@qq.com>
2023-06-07 17:20:03 -0700

    Update args.py

--- src/lmflow/args.py
+++ src/lmflow/args.py
@@ -130,6 +130,10 @@ class ModelArguments:
     tokenizer_name: Optional[str] = field(
         default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
     )
+    trust_remote_code: bool = field(
+        default=True,
+        metadata={"help": "Avoid errors for code trust."},
+    )
     cache_dir: Optional[str] = field(
         default=None,
         metadata={"help": "Where do you want to store the pretrained models downloaded from huggingface.co"},
